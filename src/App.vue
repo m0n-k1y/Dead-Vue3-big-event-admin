36ae2dfc9c0931a5a522d1b27920ef62cc86e0b3
m0_nk_1y <hcjhcjjjjj@gmail.com>
2024-03-09 23:57:01 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,37 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-console.log(123)
-console.log(123)
-console.log(123)
+import { useRouter, useRoute } from 'vue-router'
+import { useUserStore, useCounterStore } from '@/stores'
+const router = useRouter()
+const route = useRoute()
+
+const counterStore = useCounterStore()
+const userStore = useUserStore()
+
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div>
+    <router-view></router-view>
+    <hr />
+    我是APP
+    <Test-demo></Test-demo>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <el-button @click="$router.push('/home')">跳首页</el-button>
+    <el-button @click="goList">跳列表页</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('dskfjskdfjsdkfj')">登录</el-button>
+    <el-button @click="userStore.removeToken()">退出</el-button>
+    <hr />
+    {{ counterStore.count }}
+    <el-button @click="counterStore.add(2)">加法</el-button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
+@/stores/modules/user
---
+++ src/stores/index.js
@@ -0,0 +1,16 @@
+import { createPinia } from 'pinia'
+import persist from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(persist)
+
+export default pinia
+
+// import { useUserStore } from './modules/user'
+// export { useUserStore }
+
+// import { useCounterStore } from './modules/counter'
+// export { useCounterStore }
+
+export * from './modules/user'
+export * from './modules/counter'
